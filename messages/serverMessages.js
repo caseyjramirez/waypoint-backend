f0020176ecf9969d67c73ccd1e80bf602388b531
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-25 12:10:06 -0500

    Complete route for posting new user

--- messages/serverMessages.js
+++ messages/serverMessages.js
@@ -1,5 +1,7 @@
 const badRequest = 'hmm... something went wrong. Try again!';
 const userAlreadyExists = "A user with that email already has an account."
+const internalServerError = 'Internal Server Error, come back and try again!'
+const success = 'Success!'
 
 
-module.exports = {badRequest, userAlreadyExists}
+module.exports = {badRequest, userAlreadyExists, success, internalServerError}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt')
 const User = require('../models/user.schema');
 const validateNewUser = require('../validation/user.validation');
-const {badRequest, userAlreadyExists} = require('../messages/serverMessages');
+const {badRequest, userAlreadyExists, success, internalServerError} = require('../messages/serverMessages');
 
 router.get('/', async (req, res) => {
     const users = await User.find()
@@ -12,13 +13,29 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     const body = req.body;
 
+    // checking if user exists
     let user = await User.findOne({email: body.email})
     if(user) return res.status(409).send(userAlreadyExists)
 
+    // validating input
     const {error} = validateNewUser(req.body);
     if(error) return res.status(400).send(badRequest);
 
-    res.send(error)
+    // creating new user obj
+    user = new User(body)
+
+    // hashing the pw
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    try {
+        user.save();
+        res.send(success)
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send(internalServerError)
+    }
 })
 
 module.exports = router
